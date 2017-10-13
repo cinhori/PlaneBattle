ed0e234d0582040e2f0954d16fb27a3274ba5441
cinhori <lilei_1993@126.com>
2017-10-13 14:59:28 +0800

    控制玩家飞机左右移动

--- starter/main.py
+++ starter/main.py
@@ -9,6 +9,7 @@
 # @Contact : lilei93s@163.com
 
 import pygame
+from pygame.locals import *
 import time
 
 '''
@@ -24,11 +25,34 @@ def main():
 
     hero = pygame.image.load("../photo/hero1.png").convert()
 
+    x = 210
+    y = 700
     # 把背景图片放到窗口中显示
     while True:
         screen.blit(background, (0, 0))
-        screen.blit(hero, (210, 700))
+        screen.blit(hero, (x, y))
         pygame.display.update()
+
+        # 获取事件，比如按键等
+        for event in pygame.event.get():
+
+            # 判断是否是点击了退出按钮
+            if event.type == pygame.locals.QUIT:
+                print("exit")
+                exit()
+            # 判断是否是按下了键
+            elif event.type == KEYDOWN:
+                # 检测按键是否是a或者left
+                if event.key == K_a or event.key == K_LEFT:
+                    print('left')
+                    x -= 5
+                # 检测按键是否是d或者right
+                elif event.key == K_d or event.key == K_RIGHT:
+                    print('right')
+                    x += 5
+                # 检测按键是否是空格键
+                elif event.key == K_SPACE:
+                    print('space')
 
         time.sleep(0.01)
 
